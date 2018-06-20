c484535a5a57a483bd86335a8f9c87fa0b3f77a4
luoji <luoji@huored.com>
2018-06-20 10:46:11 +0800

    bug fix

--- sql/daoora.py
+++ sql/daoora.py
@@ -417,7 +417,6 @@ class DaoOra(object):
                        and a.object_name = d.index_name(+)
                        and a.object_owner = d.OWNER(+)""" )
                 eqResult = crPr.fetchall()
-                print(eqResult)
                 if len(eqResult) <= 2 and not (row[1] is not None for row in eqResult):
                     sqlContent = sqlContent
                 else:
