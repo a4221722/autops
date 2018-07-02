edc584eec92c36dc0a8c5ca1e2a012f5bd52c182
luoji <luoji@huored.com>
2018-07-02 18:09:16 +0800

    bug fix

--- sql/check_login_middleware.py
+++ sql/check_login_middleware.py
@@ -12,7 +12,7 @@ class CheckLoginMiddleware(object):
         检查是否公网访问
         """
         remoteIp = request.META.get('HTTP_X_FORWARDED_FOR',request.META['REMOTE_ADDR'])
-        if not remoteIp.startswith('192.168') and not remoteIp.startswith('10.') and request.path not in ('/login/','/logout/', '/authenticate/') and not re.match(r'^/detail/(?P<workflowId>[0-9]+)/$',request.path) and not re.match(r'^/execute/$',request.path) and not re.match(r'^/cancel/$',request.path):
+        if not remoteIp.startswith('192.168') and not remoteIp.startswith('10.') and request.path not in ('/login/','/logout/', '/authenticate/','/allworkflow/') and not re.match(r'^/detail/(?P<workflowId>[0-9]+)/$',request.path) and not re.match(r'^/execute/$',request.path) and not re.match(r'^/cancel/$',request.path):
             return HttpResponseNotFound()
         if request.session.get('login_username', False) in (False, '匿名用户'):
             #以下是不用跳转到login页面的url白名单
